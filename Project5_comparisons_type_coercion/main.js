d569169cf983c8069dbd032ca9fd276ca76d38f7
calanm <64583372+calanm@users.noreply.github.com>
2020-10-21 09:18:19 -0400

    Add files via upload

---
+++ Project5_comparisons_type_coercion/main.js
@@ -0,0 +1,15 @@
+document.write(typeof("Hi"))
+
+var exp = "10" + 2
+document.write(exp)
+
+var a = 10
+var b = 11
+var check = (a === b)
+
+document.write(10==10)
+document.write(12>20)
+document.write(12<20)
+document.write(12 = 12 && 12<20)
+document.write(12 = 12 || 12 < 5)
+document.write(10!=5)
