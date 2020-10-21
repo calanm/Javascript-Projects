9ddc61b5cc3658bbbe5748f3083d400fb70279d5
calanm <calmcaleer@gmail.com>
2020-10-21 08:09:51 -0400

    JS Project 1

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,19 @@
+var monkey = 'This is a string'
+
+document.write(monkey)
+
+window.alert(monkey)
+
+var st1 = "Hello"
+
+var st2 = "There"
+
+var st12 = st1 + st2
+
+document.write(st12)
+
+var colors = st1.fontcolor("blue")
+
+var exp = 3 + 3
+
+//finished
