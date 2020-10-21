85af94c6f3ce9aaa89ccd058a5b450bf60ee0b9d
calanm <64583372+calanm@users.noreply.github.com>
2020-10-21 08:32:02 -0400

    Add files via upload

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,13 @@
+function My_function() {
+var hi = "Hello there"
+var colors = hi.fontcolor("Blue")
+document.getElementById("Button").innerHTML = colors
+}
+//this function turns var hi blue
+
+function myButton() {
+    var str = "Why is the sky"
+    str += " Blue"
+    document.getElementById("Concatenate").innerHTML = str
+}
+// this function concatenates to strings
